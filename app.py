e174b8962c3ec0021779c3b7d7b6b3e91bd63b5c
DNOCheck <110918538+DNOCheck@users.noreply.github.com>
2022-09-07 09:37:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ app = Flask(__name__)
 app.secret_key = "Blev"
 @app.route("/TT")
 def index():
-    flash("what is your name?")
+    
     return render_template("index.html")
 
-@app.route("/greet", methods=["POST","GET"])
+
 
 def greet():
-    flash("Hi " + str(request.form['name_input']) + " great to see you")
     return render_template("index.html")
